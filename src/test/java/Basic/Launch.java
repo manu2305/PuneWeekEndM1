946bf965a47bc3d9c75e0678358a3db85eba93b0
manikandan j <jmanikandan2305@gmail.com>
2025-01-25 16:34:36 +0530

    hello

---
+++ src/test/java/Basic/Launch.java
@@ -0,0 +1,11 @@
+package Basic;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Launch {
+@Test
+public void main() {
+	Reporter.log("hello Automation",true);
+}
+}
